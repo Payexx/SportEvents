513c3ad4ffe24fb427928bb780ec751c571f26c8
Payexx <kamiltruszkowski5@gmail.com>
2025-02-13 16:49:19 +0100

    Obsługa eventów

--- sports_registration/events/models.py
+++ sports_registration/events/models.py
@@ -8,3 +8,29 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"Profil {self.user.username}"
+
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    date = models.DateField()
+    time = models.TimeField()
+    location = models.CharField(max_length=255)
+    max_participants = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+    def available_spots(self):
+        """Zwraca liczbę wolnych miejsc"""
+        return self.max_participants - self.registrations.count()
+
+class EventRegistration(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name="registrations")
+    registration_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'event')
+
+    def __str__(self):
+        return f"{self.user.username} -> {self.event.title}"
